f686fa8a157005594cadd7bef0187349e50c2ace
edson-github <edson.maxiad@gmail.com>
2025-04-21 22:35:10 -0300

    test pre-commit

---
+++ main3.py
@@ -0,0 +1,22 @@
+# create def for calculate area circle
+
+
+def calculate_area_circle(radius):
+    pi = 3.14
+    area = pi * radius**2
+    return area
+
+
+# create def for calculate area rectangle
+def calculate_area_rectangle(length, width):
+    area = length * width
+    return area
+
+
+def soma(a, b):
+    return a + b
+
+
+print(soma(1, 2))
+
+print("modificacao 2")
